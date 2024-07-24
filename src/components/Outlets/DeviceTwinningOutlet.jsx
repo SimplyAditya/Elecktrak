284ce1e16ba62c8cd7a56156647ac3e5208237aa
Shradhaaggarwal <shradhaaggarwal1402@gmail.com>
2024-07-24 22:49:36 +0530

    more changes on floor mapping

--- src/components/Outlets/DeviceTwinningOutlet.jsx
+++ src/components/Outlets/DeviceTwinningOutlet.jsx
@@ -3,13 +3,18 @@ import React from "react";
 export const DeviceTwinningOutlet = () => {
   return (
     <div className="w-full h-full overflow-y-auto rounded-2xl bg-white flex flex-wrap p-4 pt-2 justify-start items-start text-5xl outfit-font text-dashboardBlue">
-      <div className="h-full w-2/12 rounded-[20px] bg-white shadow-md drop-shadow-sm shadow-slate-300 text-dashboardBlue px-2 py-4 flex flex-row justify-start items-start outfit-font">
+      <div className="h-full w-72 rounded-[20px] bg-white shadow-md drop-shadow-sm shadow-slate-300 text-dashboardBlue px-2 py-4 flex flex-row justify-start items-start outfit-font  me-2">
         <div className="h-12 w-full rounded-[5px] bg-black shadow-md drop-shadow" >  
         </div>
         </div>
-      <div className="h-full w-7/12  rounded-[20px] bg-white shadow-md drop-shadow-sm shadow-slate-300 text-dashboardBlue px-2 py-4 flex flex-row justify-start items-start outfit-font"></div>
+      <div className="h-full w-7/12  rounded-[20px] bg-white shadow-md drop-shadow-sm shadow-slate-300 text-dashboardBlue px-2 py-4 flex flex-row justify-start items-start outfit-font me-2">
       </div>
-   
+
+      <div className="h-full w-2/12 rounded-[20px] bg-white shadow-md drop-shadow-sm shadow-slate-300 text-dashboardBlue px-2 py-4 flex flex-row justify-start items-start outfit-font  me-2"></div>
+
+
+      </div>
+      
  
   );
 };
